0286ce2e0a285d31d349ce61d497ad0cf6baef58
mounirrr <kamelfekkar4@gmail.com>
2025-09-01 02:02:06 +0100

    Delete qamis gamms/ldpage directory

--- qamis gamms/ldpage/ldpage/functions/server.js
+++
@@ -1,5 +0,0 @@
-// This file is a wrapper to run your existing server.js as a Netlify function.
-const serverless = require('serverless-http');
-const app = require('../server.js'); // Assuming your express app is exported from server.js
-
-module.exports.handler = serverless(app);
